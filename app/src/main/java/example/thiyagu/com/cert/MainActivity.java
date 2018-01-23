5118e5d99f476af391ef2735eb42801724de0336
thiyagu <thiyagu@programmer.net>
2018-01-23 18:33:57 +0530

    From android studio

--- app/src/main/java/example/thiyagu/com/cert/MainActivity.java
+++ app/src/main/java/example/thiyagu/com/cert/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if(validateAppSignature())
         {
-            Toast.makeText(getApplicationContext(), "Securtiy Verified", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Securtiy Verified Success", Toast.LENGTH_LONG).show();
 
         }
         else
